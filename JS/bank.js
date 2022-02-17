7c38b6f609f0277347d8e8bd0f70d8ca910fd7f8
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-02-17 11:02:21 +0600

    add html and js file

---
+++ JS/bank.js
@@ -0,0 +1,56 @@
+
+document.getElementById('calculate-btn').addEventListener('click', function(){
+     //Expenses part
+
+    const  foodInput = document.getElementById('food-input').value;
+    const  rentInput = document.getElementById('rent-input').value;
+    const  clothesInput = document.getElementById('clothes-input').value;
+    const  totalAmount = parseInt(foodInput) + parseInt(rentInput) + parseInt(clothesInput);
+     const totalExpenses = document.getElementById('total-expenses');
+      totalExpenses.innerText = totalAmount;  
+
+        //    calculation part 
+    const incomeInput = document.getElementById('income-input');
+    const incomeAmount = incomeInput.value;
+    const totalBalance = document.getElementById('total-balance');
+    const totalBalanceAmount = totalBalance.innerText;
+    totalBalance.innerText = parseInt(incomeAmount) - parseInt(totalAmount);
+
+
+
+    
+});
+
+document.getElementById('save-btn').addEventListener('click', function(){
+    const saveIncomeInput = document.getElementById('income-input');
+    const saveIncomeAmount = saveIncomeInput.value;
+    
+
+})
+
+
+
+
+// document.getElementById('calculate-btn').addEventListener('click', function(){
+
+    
+    
+      
+//         const  foodInput = document.getElementById('food-input').value;
+//         const  rentInput = document.getElementById('rent-input').value;
+//         const  clothesInput = document.getElementById('clothes-input').value;
+//         const  totalAmount = parseInt(foodInput) + parseInt(rentInput) + parseInt(clothesInput);
+//          const totalExpenses = document.getElementById('total-expenses');
+//           totalExpenses.innerText = totalAmount; 
+        
+          
+//       const incomeInput = document.getElementById('income-input');
+//       const incomeAmount = incomeInput.value;
+//       const totalBalance = document.getElementById('total-balance');
+//       const totalBalanceAmount = totalBalance.innerText;
+//       totalBalance.innerText = parseInt(incomeAmount) - parseInt(totalAmount);
+
+// })
+
+
+
